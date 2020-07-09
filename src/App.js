8703015f29fa76a7901d0413e338dbb857ee3aba
Jerry Xu <jerryxwang9@gmail.com>
2020-07-09 11:11:59 -0700

    testing 2

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ import './App.css';
 
 function App() {
   return (
-    <p>testtttttttttingggggggg</p>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>testingggggg plzzz work</p>
     </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
